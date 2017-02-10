10eccbd822e93793c2515dd833cc6bfc05fce075
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-02-10 16:53:23 +0000

    Refactored server and package entry route.

--- web/app.js
+++ web/app.js
@@ -1,62 +1,60 @@
 'use strict';
 
-const express = require('express');
+const fs = require('fs');
+const nib = require('nib');
+const path = require('path');
 const stylus = require('stylus');
 const morgan = require('morgan');
+const express = require('express');
 const winston = require('winston');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const fs = require('fs');
-const nib = require('nib');
-
-const path = require('path');
 
 // Routers
 const indexRouter = require('./routes/index');
 
 // Express
-const server = express();
-const dev = (server.get('env') === 'development');
+const app = express();
+const dev = (app.get('env') === 'development');
 
 // Make the HTML output pretty in development.
-server.locals.pretty = dev;
+app.locals.pretty = dev;
 
-// Setup morgan logger.
-if (server.get('env') === 'development') {
-    server.use(morgan('dev'));  
-} else if (server.get('env') === 'production') {
-
-    // Create a write stream (in append mode) 
+if (dev) {
+    // Just output to console only.
+    app.use(morgan('dev'));
+} else {
+    // Create a write stream (in append mode)
     const accessLogStream = fs.createWriteStream(
         path.join(__dirname, 'logs', 'access.log'),
         { flags: 'a' }
     );
 
-    server.use(morgan('common', {stream: accessLogStream}));
+    app.use(morgan('common', {stream: accessLogStream}));
 }
 
 // Setup winston logger.
 winston.add(winston.transports.File, {
-    filename: path.join('logs', 'server.log'),
+    filename: path.join('logs', 'app.log'),
     handleExceptions: true,
     humanReadableUnhandledException: true
 });
 
 // Set template engine to jade/pug.
-server.set('view engine', 'pug');
-server.set('views', path.join(__dirname, 'views', 'templates'));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views', 'templates'));
 
 // Parse application/x-www-form-urlencoded
-server.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Parse application/json
-server.use(bodyParser.json());
+app.use(bodyParser.json());
 
 // Parse cookies
-server.use(cookieParser());
+app.use(cookieParser());
 
 // Stylus middleware
-server.use(stylus.middleware({
+app.use(stylus.middleware({
     src: path.join(__dirname, 'views'),
     dest: path.join(__dirname, 'public'),
     compile: (str, path) => {
@@ -68,22 +66,22 @@ server.use(stylus.middleware({
 }));
 
 // Set base directory for path requests
-server.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use indexRouter
-server.use('/', indexRouter);
+app.use('/', indexRouter);
 
 // Error handler: 404 handler
-server.use((req, res, next) => {
+app.use((req, res, next) => {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // Error handler: No stacktraces leaked to user production mode.
-server.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.send(dev ? err : {});
 });
 
-module.exports = server;
+module.exports = app;
--- web/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const server = require('./server');
-const PORT = 8080;
-
-// Start server.
-server.listen(PORT, () => {
-    console.log('server is running in http://localhost at port ' + 8080); 
-});
