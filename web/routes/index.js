427b6e53c1b4453737d0033067e9ffe3fce8a5d8
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-02-12 07:31:47 +0000

    Proxies nodebb + add package.

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const router = require('express').Router();
+const proxy = require('express-http-proxy');
 
 router.get('/', (req, res) => {
     res.render('index');
@@ -17,5 +18,7 @@ router.get('/boards/:id', (req, res) => {
 
     res.render('board', options);
 });
+
+router.get('/forum', proxy('//localhost:4567'));
 
 module.exports = router;
