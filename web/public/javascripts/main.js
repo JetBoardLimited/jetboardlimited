1f3923dc8de77428a9a9bf73fcc72cba3174d5c8
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-02-12 00:52:03 +0000

    Milestone.

--- web/public/javascripts/main.js
+++ web/public/javascripts/main.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', animateIntro);
 document.addEventListener('DOMContentLoaded', animateManufacturersList);
+document.addEventListener('DOMContentLoaded', createBoardMediaGallery);
 
 function animateIntro() {
+    var element = document.getElementById('intro-word');
+
+    if (element === undefined || element === null) {
+        return;
+    }
+
     var words = [
         'sell', 'buy', 'hire', 'discuss', 'review'
     ];
@@ -9,7 +16,7 @@ function animateIntro() {
     var index = 0;
 
     setInterval(function () {
-        document.getElementById('intro-word').textContent = words[index % words.length];
+        element.textContent = words[index % words.length];
         index = (index + 1) % words.length;
     }, 1000);
 }
@@ -25,3 +32,19 @@ function animateManufacturersList() {
     });
 }
 
+function createBoardMediaGallery() {
+     $('.board-media-gallery-preview').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.board-media-gallery-controls'
+    });
+
+    $('.board-media-gallery-controls').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.board-media-gallery-preview',
+        focusOnSelect: true
+    });
+}
--- web/routes/index.js
+++ web/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.get('/boards', (req, res) => {
+    res.render('boards');
+});
+
+router.get('/boards/:id', (req, res) => {
+    const options = {
+      board: req.params.id
+    };
+
+    res.render('board', options);
+});
+
 module.exports = router;
