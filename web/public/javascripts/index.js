e4745b3487953e565cbec239b52cce54b08933e0
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-05-02 18:40:53 +0100

    Interval intro images.

--- web/public/javascripts/index.js
+++ web/public/javascripts/index.js
@@ -15,9 +15,25 @@ function animateIntro() {
 
     var index = 0;
 
+    var introElement = document.querySelector('.intro');
+
+    if (introElement === undefined || introElement === null) {
+        return;
+    }
+
+    var introWords = [
+        'main-background.png',
+        'main-background-1.png',
+        'main-background-2.jpg'
+    ];
+
+    var introIndex = 0;
+
     setInterval(function () {
         element.textContent = words[index % words.length];
         index = (index + 1) % words.length;
+        introElement.style.backgroundImage = 'url(/images/'+introWords[introIndex % introWords.length]+')';
+        introIndex = (introIndex + 1) % introWords.length;
     }, 1000);
 }
 
@@ -33,18 +49,4 @@ function animateManufacturersList() {
 }
 
 function createBoardMediaGallery() {
-     $('.board-media-gallery-preview').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.board-media-gallery-controls'
-    });
-
-    $('.board-media-gallery-controls').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        asNavFor: '.board-media-gallery-preview',
-        focusOnSelect: true
-    });
 }
