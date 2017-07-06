fbe2c1815620d5a4399c16b345fc70a73950b66b
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-07-06 17:00:01 +0100

    Refactors disqus comments into a block.

--- web/routes/boards.js
+++ web/routes/boards.js
@@ -8,7 +8,9 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     const options = {
-      board: req.params.id
+      board: req.params.id,
+      disqusUrl: process.env.DISQUS_URL,
+      serverHostUrl: process.env.SERVER_HOST_URL
     };
 
     res.render('boards/' + options.board, options);
