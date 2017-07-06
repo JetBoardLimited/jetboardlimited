50827b22bbe1b9948911a31ec07da42a97571c9b
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-07-06 19:34:42 +0100

    Use cli mocha rather than gulp.

--- web/gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
-
-gulp.task('test', function() {
-    return gulp.src(['tests/test-*.js'], { read: false })
-        .pipe(mocha({
-            reporter: 'spec'
-        }));
-});
--- web/tests/routes.test.js
+++ web/tests/routes.test.js
@@ -1,10 +1,10 @@
-const server = require('../server');
+const app = require('../app');
 const expect = require('expect');
 const request = require('supertest');
 
 describe('GET /', () => {
     it('should return a response', (done) => {
-        request(server)
+        request(app)
             .get('/')
             .expect(200, done);
     });
