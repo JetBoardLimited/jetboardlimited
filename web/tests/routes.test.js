57b8783b4615965f319164b11f48fcfb6acec8ec
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-07-06 20:07:17 +0100

    Adds basic routing tests.

--- web/tests/routes.test.js
+++ web/tests/routes.test.js
@@ -2,10 +2,23 @@ const app = require('../app');
 const expect = require('expect');
 const request = require('supertest');
 
-describe('GET /', () => {
-    it('should return a response', (done) => {
-        request(app)
-            .get('/')
-            .expect(200, done);
+const routes = [
+    '/',
+    '/boards',
+    '/lessons',
+    '/partnerships',
+    '/about',
+    '/under-construction'
+];
+
+describe('Routing', () => {
+    routes.forEach((route) => {
+        describe('GET /', () => {
+            it('should return a response', (done) => {
+                request(app)
+                    .get(route)
+                    .expect(200, done);
+            });
+        });
     });
 });
