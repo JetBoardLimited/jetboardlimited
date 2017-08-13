b3ff9eed2c491ec1b26d503ffb23907613b4ea89
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-08-13 10:08:19 +0100

    Adds restore/backup scripts.

---
+++ backup.sh
@@ -0,0 +1,3 @@
+docker cp jbl-forum:/nodebb/config.json ./data/forum/config.json
+docker cp jbl-forum:/nodebb/uploads ./data/forum
+docker cp jbl-redis:/data ./data/redis
---
+++ restore.sh
@@ -0,0 +1,3 @@
+docker cp ./data/forum/config.json jbl-forum:/nodebb/config.json
+docker cp ./data/forum/uploads jbl-forum:/nodebb/uploads
+docker cp ./data/redis jbl-redis:/data
