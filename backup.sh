2f89c209d150e45f4ddc2c2a6ef89c7a4c61aa82
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-10-21 14:46:14 +0100

    devops: backup everything

--- backup.sh
+++ backup.sh
@@ -1,5 +1,4 @@
 mkdir -p ./data
 
-docker cp jbl-forum:/nodebb/config.json ./data
-docker cp jbl-forum:/nodebb/uploads ./data
+docker cp jbl-forum:/nodebb ./data
 docker cp jbl-redis:/data/dump.rdb ./data
