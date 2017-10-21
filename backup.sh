76b4656ca5b689bd3f416a2d66d8b97862d353b2
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-10-21 22:45:31 +0100

    devops: fixes backing up wrong directory

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 mkdir -p ./data
 
 docker cp jbl-forum:/nodebb/config.json ./data
-docker cp jbl-forum:/nodebb/uploads ./data
+docker cp jbl-forum:/nodebb/public/uploads ./data
 docker cp jbl-redis:/data/dump.rdb ./data
--- restore.sh
+++ restore.sh
@@ -1,5 +1,5 @@
 mkdir -p ./data
 
 docker cp ./data/config.json jbl-forum:/nodebb
-docker cp ./data/uploads jbl-forum:/nodebb
+docker cp ./data/uploads jbl-forum:/nodebb/public
 docker cp ./data/dump.rdb jbl-redis:/data
