44ee0a7afeea95dae9488926dfeaa2cf677716fe
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-10-22 02:12:59 +0100

    replaces scripts with one do it all script.

--- restore.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ./data
-
-docker cp ./data/config.json jbl-forum:/nodebb
-docker cp ./data/uploads jbl-forum:/nodebb/public
-docker cp ./data/dump.rdb jbl-redis:/data
---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [ -z $@ ]; then
+  echo "Needs argument ('prod' or dev')."
+  exit 1
+fi
+
+set -e
+
+echo "Stopping containers..."
+docker-compose stop
+
+echo "Saving data..."
+TMPDIR="$(mktemp -d)"
+docker cp jbl-forum:/nodebb/public/uploads $TMPDIR
+docker cp jbl-redis:/data/dump.rdb $TMPDIR
+mkdir -p data
+tar -czf "./data/backup-$(date +%F_%R).tar.gz" -C $TMPDIR .
+
+echo "Updating source..."
+git pull origin
+
+echo "Starting containers..."
+docker-compose -f docker-compose.yml -f docker-compose-${@}.yml up -d
+
+echo "Restoring data..."
+docker cp $TMPDIR/dump.rdb jbl-redis:/data
+docker cp $TMPDIR/uploads jbl-forum:/nodebb/public
+
+rm -rf $TMPDIR
+
+echo "Finished!"
