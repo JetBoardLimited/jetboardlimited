872af3b04e6126d855a06d0fa9a1e3f62684a708
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2017-10-22 03:05:00 +0100

    fixes update script

--- update.sh
+++ update.sh
@@ -20,15 +20,17 @@ tar -czf "./data/backup-$(date +%F_%R).tar.gz" -C $TMPDIR .
 echo "Updating source..."
 git pull origin
 
-
-echo "Starting containers..."
+echo "Building containers..."
 docker-compose down
-docker-compose -f docker-compose.yml -f docker-compose-${@}.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose-${@}.yml up -d --build
+docker-compose stop
 
 echo "Restoring data..."
 docker cp $TMPDIR/dump.rdb jbl-redis:/data
 docker cp $TMPDIR/uploads jbl-forum:/nodebb/public
-
 rm -rf $TMPDIR
+
+echo "Starting containers..."
+docker-compose start
 
 echo "Finished!"
